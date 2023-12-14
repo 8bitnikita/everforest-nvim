e37d658eb2bd85b4b2bf7abef0aed12723ad429d
Nikita <geekneek28@gmail.com>
2023-12-14 18:02:56 +0200

    Update highlights.lua

--- lua/everforest/highlights.lua
+++ lua/everforest/highlights.lua
@@ -975,7 +975,7 @@ highlights.generate_syntax = function(palette, options)
     TroubleCode = { link = "Grey" },
 
     -- nvim-telescope/telescope.nvim
-    TelescopeMatching = syntax_entry(palette.green, palette.none, { styles.bold }),
+    TelescopeMatching = syntax_entry(palette.yellow, palette.none, { styles.bold }),
     TelescopeBorder = { link = "Grey" },
     TelescopePromptPrefix = { link = "Orange" },
     TelescopeSelection = { link = "DiffAdd" },
