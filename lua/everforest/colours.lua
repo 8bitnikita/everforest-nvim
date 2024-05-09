56bd668f95bb6b3b2991d29ea3a45d4c2de1e9f9
Nikita Ganzha <nk8bnj@gmail.com>
2024-05-09 17:23:22 +0300

    update colors

--- lua/everforest/colours.lua
+++ lua/everforest/colours.lua
@@ -112,13 +112,14 @@ local base_palette = {
     none = "NONE",
   },
   dark = {
-    blue = "#E1C891", --done
+    -- fg = "#E1C891", --done
+    fg = "#95ACAC", --done
     red = "#EE6060", --done
     orange = "#F38E61", --done
     yellow = "#EFBD58", --done
     green = "#A9CD73", --done
     aqua = "#69CA81", --done
-    fg = "#4DC5B6", --done
+    blue = "#4DC5B6", --done
     purple = "#D182D8",
     grey0 = "#7a8478",
     grey1 = "#859289",
